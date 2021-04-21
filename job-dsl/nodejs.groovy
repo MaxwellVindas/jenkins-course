15a731a5c6543c175b4de1c0661e644eb6d0f53b
MaxwellVindas <maxwell.vindas@ucr.ac.cr>
2021-04-21 12:17:28 -0600

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('git:github.com/MaxwellVindas/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('MaxwellVindas')
+            node / gitConfigEmail('maxwell.vindas@ucr.ac.cr')
         }
     }
     triggers {
